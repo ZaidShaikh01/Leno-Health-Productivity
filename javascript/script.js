9969977b727daa2f299a2454943308ed44d1d3b1
ZaidShaikh <szaid5146@gmail.com>
2026-02-27 17:02:21 +0530

    Navbar added

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+document.addEventListener(
+    'DOMContentLoaded',
+    function(){
+        //Mobile menu
+        const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
+        const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
+        toggleButton.addEventListener(
+            'click',
+            function(){
+                mobileMenu.classList.toggle('active');
+            }
+        )
+    }
+)
+
+// Navigation background on scroll
+window.addEventListener('scroll',
+    function(){
+        const navbar = document.querySelector('.navbar');
+        if(this.window.scrollY>0){
+            navbar.classList.add('navbar--scroll');
+        }
+        else{
+            navbar.classList.remove
+            ('navbar--scroll');
+        }
+    }
+)
